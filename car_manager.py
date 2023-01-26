ccfc5a1ace963459643a415597b9162ed382c60d
KesterDaniel <kesterdan17@gmail.com>
2023-01-26 12:14:24 +0100

    generating random moving cars across the screen

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,18 @@
+import random
+from turtle import Turtle
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("square")
+        self.penup()
+        self.shapesize(stretch_len=1.5, stretch_wid=0.8)
+        self.color(random.choice(COLORS))
+        self.goto(340, random.randint(-250, 250))
+        self.setheading(180)
+        self.move_distance = STARTING_MOVE_DISTANCE
--- main.py
+++ main.py
@@ -9,13 +9,17 @@ screen.setup(width=600, height=600)
 screen.tracer(0)
 
 player = Player()
-
 screen.listen()
 screen.onkey(key="Up", fun=player.player_move)
 
 game_is_on = True
+cars = []
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    car = CarManager()
+    cars.append(car)
+    for car in cars:
+        car.forward(car.move_distance)
     if player.ycor() == player.finish_line:
         player.player_start()
