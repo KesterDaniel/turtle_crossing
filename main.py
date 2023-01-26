649472fd634a10bf18cbebeac06e892fed7f9c07
KesterDaniel <kesterdan17@gmail.com>
2023-01-26 12:45:00 +0100

    detecting player collision with car

--- main.py
+++ main.py
@@ -13,13 +13,21 @@ screen.listen()
 screen.onkey(key="Up", fun=player.player_move)
 
 game_is_on = True
+
 cars = []
 while game_is_on:
     time.sleep(0.1)
     screen.update()
     car = CarManager()
+    car.forward(car.move_distance)
     cars.append(car)
     for car in cars:
         car.forward(car.move_distance)
+
+    # detect if player reaches the other side
     if player.ycor() == player.finish_line:
         player.player_start()
+
+    for car in cars:
+        if player.distance(car) < 20:
+            game_is_on = False
